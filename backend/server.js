aad031ba1fb21dba07d1079614c16e26ecae14d6
peter <peter840101@gmail.com>
2023-06-15 22:39:45 +0800

    generateToken function

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const userRoutes = require('./routes/userRoutes');
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("api running")
 })
